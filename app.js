bc22d695681831d7728682d1fa46891f4de69d1f
bhabin khadka <bhabin@bhabin-mac.local>
2022-05-09 07:10:56 +0545

    location update in chat app

--- app.js
+++ app.js
@@ -4,23 +4,16 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 
 const connectDB = require('./db/db_connection')
-const { NotFoundError } = require('./errors/error')
 const app = express()
 const port = 3000
 const errorHandler = require('./middleware/error_handler')
 const pageNotFound = require('./middleware/page_not_found')
 const auth = require('./routes/auth')
-const task = require('./routes/task')
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Express')
-})
+const message = require('./routes/message')
 
 app.use(express.json())
 app.use(auth)
-app.use(task)
-// app.use(task)
+app.use(message)
 
 app.use(pageNotFound)
 app.use(errorHandler)
